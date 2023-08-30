0345b8819f02e505363cef5e303dad143c960ef9
theo karam <karam_theo@Hotmail.com>
2023-08-30 16:04:33 +0300

    Added the Projects part

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Project = ({ img, title, description }) => {
+const Project = ({ img, title }) => {
   return (
     <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#001b5e]">
       <img
@@ -12,7 +12,7 @@ const Project = ({ img, title, description }) => {
         <h3 className="text-2xl font-bold text-white tracking-wider text-center">
           {title}
         </h3>
-        <p className="pb-4 pt-2 text-white text-center">{description}</p>
+        <p className="pb-4 pt-2 text-white text-center">React</p>
         <a href="/">
           <p className="text-center p-3 rounded-lg bg-white text-gray-700 font-bold cursor-pointer text-lg">
             More Info
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,29 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Project from "./Project";
 import facebookImg from "../assets/facebook.png";
 import googleImg from "../assets/google.png";
 import railwayImg from "../assets/railway.png";
 import youtubeImg from "../assets/youtube.png";
 
-const ProjectsData = () => {
-  const apiEndPoint = "http://127.0.0.1:8000/api/data/";
-  return fetch(apiEndPoint);
-};
-
 const Projects = () => {
-  // Commented section below is a example of how to fetch data from a Django API
-  // const [projects, setProjects] = React.useState([]);
-
-  // React.useEffect(() => {
-  //   ProjectsData()
-  //     .then((response) => response.json()) // Parse the response data as JSON
-  //     .then((data) => {
-  //       setProjects(data); // Log the data to the console
-  //     })
-  //     .catch((error) => {
-  //       console.error("Error fetching data:", error);
-  //     });
-  // }, []);
+  const projects = [
+    {
+      img: facebookImg,
+      title: "Facebook App",
+    },
+    {
+      img: googleImg,
+      title: "Google App",
+    },
+    {
+      img: railwayImg,
+      title: "Railway App",
+    },
+    {
+      img: youtubeImg,
+      title: "Youtube App",
+    },
+  ];
 
   return (
     <div id="projects" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
@@ -36,11 +36,11 @@ const Projects = () => {
         officia, consectetur sed sapiente rerum dolores nostrum ipsum molestias
         similique quaerat inventore! Dolor.
       </p>
+
       <div className="grid sm:grid-cols-2 gap-12">
-        <Project img={facebookImg} title="Facebook App" />
-        <Project img={googleImg} title="Google App" />
-        <Project img={railwayImg} title="Railway App" />
-        <Project img={youtubeImg} title="Youtube App" />
+        {projects.map((project) => (
+          <Project img={project.img} title={project.title} />
+        ))}
       </div>
     </div>
   );
